f8babf6bd4bae281c4b12a540b2a0519c9a367ad
Tarun kurapati <87386296+TarunKurapatiChowdary@users.noreply.github.com>
2022-04-26 10:05:30 +0530

    Add files via upload

---
+++ Formula.java
@@ -0,0 +1,12 @@
+class Formula
+  {
+    public static void main(String args[])
+    {
+    int a,b,c,d;
+   a=10;
+    b=12;
+    c=13;
+    d=(b*b)-(4*a*c);
+System.out.println("output is"+d);
+  }
+  }
