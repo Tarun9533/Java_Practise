d1daed13f983fb982422fcc9fae1acb2b0cbb944
Tarun kurapati <87386296+TarunKurapatiChowdary@users.noreply.github.com>
2022-05-03 10:13:51 +0530

    Add files via upload

---
+++ Arthametic_Expression/Arrayindex.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Arrayindex
+  {
+    public static void main(String args[])
+    {
+      int i;
+      int a[]=new int[5];
+      Scanner sc=new Scanner (System.in);
+      System.out.println("Enter elements");
+      for(i=0;i<5;i++)
+        {
+          System.out.println(a(b));
+        }
+    }
+  }
+  
---
+++ Arthametic_Expression/Vote.java
@@ -0,0 +1,21 @@
+class invalidage extends Execption
+{
+    void invalidage Exception(String age)
+  {
+      Super(age);
+    }
+  
+class Vote
+  {
+    public int check age(int age);
+     if(age<18)
+       {
+        throw new invalidage("you are not eligible to vote");
+       }
+ else
+      {
+       System.out.println("Eligible to vote");
+      }
+}
+
+      
